39741618d7b982445eb6f040d6fc460468ef9395
Kibin <jacobyoo00@hotmail.com>
2021-03-31 22:32:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,13 @@
 # code your #valid_move? method here
 # require 'pry'
+def position_taken?(board, index)
+if board[index] == " " || board[index] == "" || board[index] == nil
+  return false
+else
+  return true
+end
+end
+
 def valid_move?(board, index)
 # binding.pry
 if position_taken?
