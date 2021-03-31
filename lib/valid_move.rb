02bcc7b219737234796195821d752882a26a0137
Kibin <jacobyoo00@hotmail.com>
2021-03-31 21:52:17 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,7 @@
 # code your #valid_move? method here
+require 'pry'
+def valid_move?(board, index)
+binding.pry
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
